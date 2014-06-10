5be01ef87dcca992b8e427b27da582048c36d822
chandermani <chandermani@outlook.com>
2014-06-10 21:36:13 +0530

    SPA work in progress

--- chapter2/app/js/app.js
+++ chapter2/app/js/app.js
@@ -1,5 +1,11 @@
 'use strict';
 
-angular.module('app', ['7minWorkout']);
+angular.module('app', ['ngRoute', '7minWorkout']).
+config(function ($routeProvider) {
+    $routeProvider.when('/start', { templateUrl: 'partials/start.html' });
+    $routeProvider.when('/workout', { templateUrl: 'partials/workout.html', controller: 'WorkoutController' });
+    $routeProvider.when('/finish', { templateUrl: 'partials/finish.html' });
+    $routeProvider.otherwise({ redirectTo: '/start' });
+});
 
 angular.module('7minWorkout', []);
