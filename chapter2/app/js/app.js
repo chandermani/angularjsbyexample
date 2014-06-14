2621b4de3b28ae3efd74141eeb53bd61408c798e
chandermani <chandermani@outlook.com>
2014-06-14 12:15:51 +0530

    Fixed checkpoint 5.

--- chapter2/app/js/app.js
+++ chapter2/app/js/app.js
@@ -1,11 +1,17 @@
 'use strict';
 
 angular.module('app', ['ngRoute', '7minWorkout']).
-config(function ($routeProvider) {
+config(function ($routeProvider, $sceDelegateProvider) {
     $routeProvider.when('/start', { templateUrl: 'partials/start.html' });
     $routeProvider.when('/workout', { templateUrl: 'partials/workout.html', controller: 'WorkoutController' });
     $routeProvider.when('/finish', { templateUrl: 'partials/finish.html' });
     $routeProvider.otherwise({ redirectTo: '/start' });
+
+    $sceDelegateProvider.resourceUrlWhitelist([
+      // Allow same origin resource loads.
+      'self',
+      // Allow loading from our assets domain.  Notice the difference between * and **.
+      'http://*.youtube.com/**']);
 });
 
 angular.module('7minWorkout', []);
