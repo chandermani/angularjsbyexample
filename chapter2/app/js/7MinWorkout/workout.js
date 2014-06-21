386974ca9a30aefc4704bee81a8b7453825ca02b
chandermani <chandermani@outlook.com>
2014-06-21 18:59:09 +0530

    Added exercise data.

--- chapter2/app/js/7MinWorkout/workout.js
+++ chapter2/app/js/7MinWorkout/workout.js
@@ -29,10 +29,9 @@ angular.module('7minWorkout')
           restExercise = {
               exercise: new Exercise({
                   name: "rest",
-                  title: "Rest",
-                  description: "Discription about resting :)",
+                  title: "Relax!",
+                  description: "Relax a bit!",
                   image: "img/rest.png",
-
               }),
               duration: workoutPlan.restBetweenExercise
           };
@@ -102,10 +101,11 @@ angular.module('7minWorkout')
               exercise: new Exercise({
                   name: "wallSit",
                   title: "Wall Sit",
-                  description: "Wall Sit.",
+                  description: "A wall sit, also known as a Roman Chair, is an exercise done to strengthen the quadriceps muscles.",
                   image: "img/wallsit.png",
-                  videos: [],
-                  procedure: ""
+                  videos: ["//www.youtube.com/embed/y-wV4Venusw", "//www.youtube.com/embed/MMV3v4ap4ro"],
+                  procedure: "Place your back against a wall with your feet shoulder width apart and a little ways out from the wall.\
+                              Then, keeping your back against the wall, lower your hips until your knees form right angles. "
               }),
               duration: 30
           });
@@ -113,10 +113,12 @@ angular.module('7minWorkout')
               exercise: new Exercise({
                   name: "pushUp",
                   title: "Push Up",
-                  description: "Discription about pushup.",
+                  description: "A push-up is a common exercise performed in a prone position by raising and lowering the body using the arms",
                   image: "img/pushup.png",
-                  videos: ["https://www.youtube.com/watch?v=Eh00_rniF8E", "https://www.youtube.com/watch?v=ZWdBqFLNljc", "https://www.youtube.com/watch?v=UwRLWMcOdwI", "https://www.youtube.com/watch?v=ynPwl6qyUNM", "https://www.youtube.com/watch?v=OicNTT2xzMI"],
-                  procedure: ""
+                  videos: ["//www.youtube.com/embed/Eh00_rniF8E", "//www.youtube.com/embed/ZWdBqFLNljc", "//www.youtube.com/embed/UwRLWMcOdwI", "//www.youtube.com/embed/ynPwl6qyUNM", "//www.youtube.com/embed/OicNTT2xzMI"],
+                  procedure: "Lie prone on the ground with hands placed as wide or slightly wider than shoulder width. \
+                              Keeping the body straight, lower body to the ground by bending arms at the elbows. \
+                              Raise body up off the ground by extending the arms."
               }),
               duration: 30
           });
@@ -124,10 +126,15 @@ angular.module('7minWorkout')
               exercise: new Exercise({
                   name: "crunches",
                   title: "Abdominal Crunches",
-                  description: "Abdominal Crunches.",
+                  description: "The basic crunch is a abdominal exercise in a strength-training program.",
                   image: "img/crunches.png",
-                  videos: [],
-                  procedure: ""
+                  videos: ["//www.youtube.com/embed/Xyd_fa5zoEU", "//www.youtube.com/embed/MKmrqcoCZ-M"],
+                  procedure: "Lie on your back with your knees bent and feet flat on the floor, hip-width apart.\
+                              Place your hands behind your head so your thumbs are behind your ears.\
+                              Hold your elbows out to the sides but rounded slightly in.\
+                              Gently pull your abdominals inward.\
+                              Curl up and forward so that your head, neck, and shoulder blades lift off the floor.\
+                              Hold for a moment at the top of the movement and then lower slowly back down."
               }),
               duration: 30
           });
@@ -135,10 +142,13 @@ angular.module('7minWorkout')
               exercise: new Exercise({
                   name: "stepUpOntoChair",
                   title: "Step Up Onto Chair",
-                  description: "Step Up Onto Chair.",
+                  description: "Step exercises are ideal for building muscle in your lower body.",
                   image: "img/stepUpOntoChair.jpeg",
-                  videos: [],
-                  procedure: ""
+                  videos: ["//www.youtube.com/embed/aajhW7DD1EA"],
+                  procedure: "Position your chair in front of you.\
+                              Stand with your feet about hip width apart, arms at your sides.\
+                              Step up onto the seat with one foot, pressing down while bringing your other foot up next to it. \
+                              Step back with the leading foot and bring the trailing foot down to finish one step-up."
               }),
               duration: 30
           });
@@ -146,10 +156,14 @@ angular.module('7minWorkout')
               exercise: new Exercise({
                   name: "squat",
                   title: "Squat",
-                  description: "Squat.",
+                  description: "The squat is a compound, full body exercise that trains primarily the muscles of the thighs, hips, buttocks and quads.",
                   image: "img/squat.png",
-                  videos: [],
-                  procedure: ""
+                  videos: ["//www.youtube.com/embed/QKKZ9AGYTi4", "//www.youtube.com/embed/UXJrBgI2RxA"],
+                  procedure: "Stand with your head facing forward and your chest held up and out.\
+                              Place your feet shoulder-width apart or little wider. Extend your hands straight out in front of you.\
+                              Sit back and down like you're sitting into a chair. Keep your head facing straight as your upper body bends forward a bit. Rather than allowing your back to round, let your lower back arch slightly as you go down.\
+                              Lower down so your thighs are parallel to the floor, with your knees over your ankles. Press your weight back into your heels.\
+                              Keep your body tight, and push through your heels to bring yourself back to the starting position."
               }),
               duration: 30
           });
@@ -157,10 +171,13 @@ angular.module('7minWorkout')
               exercise: new Exercise({
                   name: "tricepdips",
                   title: "Tricep Dips On Chair",
-                  description: "Tricep Dips On Chair.",
+                  description: "A body weight exercise that targets triceps.",
                   image: "img/tricepdips.jpg",
-                  videos: [],
-                  procedure: ""
+                  videos: ["//www.youtube.com/embed/tKjcgfu44sI", "//www.youtube.com/embed/jox1rb5krQI"],
+                  procedure: "Sit up on a chair. Your legs should be slightly extended, with your feet flat on the floor.\
+                              Place your hands edges of the chair. Your palms should be down, fingertips pointing towards the floor.\
+                              Without moving your legs, bring your glutes forward off the chair.\
+                              Steadily lower yourself. When your elbows form 90 degrees angles, push yourself back up to starting position."
               }),
               duration: 30
           });
@@ -168,10 +185,13 @@ angular.module('7minWorkout')
               exercise: new Exercise({
                   name: "plank",
                   title: "Plank",
-                  description: "Plank.",
+                  description: "The plank (also called a front hold, hover, or abdominal bridge) is an isometric core strength exercise that involves maintaining a difficult position for extended periods of time. ",
                   image: "img/plank.png",
-                  videos: [],
-                  procedure: ""
+                  videos: ["//www.youtube.com/embed/pSHjTRCQxIw", "//www.youtube.com/embed/TvxNkmjdhMM"],
+                  procedure: "Get into pushup position on the floor.\
+                              Bend your elbows 90 degrees and rest your weight on your forearms.\
+                              Your elbows should be directly beneath your shoulders, and your body should form a straight line from head to feet.\
+                              Hold this position."
               }),
               duration: 30
           });
@@ -179,10 +199,11 @@ angular.module('7minWorkout')
               exercise: new Exercise({
                   name: "highKnees",
                   title: "High Knees",
-                  description: "High Knees.",
+                  description: "A form exercise that develops strength and endurance of the hip flexors and quads and stretches the hip extensors.",
                   image: "img/highknees.png",
-                  videos: [],
-                  procedure: ""
+                  videos: ["//www.youtube.com/embed/OAJ_J3EZkdY", "//www.youtube.com/embed/8opcQdC-V-U"],
+                  procedure: "Start standing with feet hip-width apart. \
+                              Do inplace jog with your knees lifting as much as possible towards your chest."
               }),
               duration: 30
           });
@@ -190,10 +211,15 @@ angular.module('7minWorkout')
               exercise: new Exercise({
                   name: "lunges",
                   title: "Lunges",
-                  description: "Lunges.",
+                  description: "Lunges are a good exercise for strengthening, sculpting and building several muscles/muscle groups, including the quadriceps (or thighs), the gluteus maximus (or buttocks) as well as the hamstrings. ",
                   image: "img/lunges.png",
-                  videos: [],
-                  procedure: ""
+                  videos: ["//www.youtube.com/embed/Z2n58m2i4jg"],
+                  procedure: "Stand erect with your feet about one shoulder width apart.\
+                              Put your hands on your hips, keep your back as straight as possible, relax your shoulders and keep your eyes facing directly ahead.\
+                              Take a large step forward with one leg.\
+                              As you step forward, lower your hips and bend your knees until they both form 90 degree angles.\
+                              Return to starting position.\
+                              Repeat with your alternate leg."
               }),
               duration: 30
           });
@@ -201,10 +227,11 @@ angular.module('7minWorkout')
               exercise: new Exercise({
                   name: "pushupNRotate",
                   title: "Pushup And Rotate",
-                  description: "Pushup And Rotate.",
+                  description: "A variation of pushup that requires you to rotate.",
                   image: "img/pushupNRotate.jpg",
-                  videos: [],
-                  procedure: ""
+                  videos: ["//www.youtube.com/embed/qHQ_E-f5278"],
+                  procedure: "Assume the classic pushup position, but as you come up, rotate your body so your right arm lifts up and extends overhead.\
+                              Return to the starting position, lower yourself, then push up and rotate till your left hand points toward the ceiling."
               }),
               duration: 30
           });
@@ -212,10 +239,13 @@ angular.module('7minWorkout')
               exercise: new Exercise({
                   name: "sidePlank",
                   title: "Side Plank",
-                  description: "Side Plank.",
+                  description: "A variation to Plank done using one hand only",
                   image: "img/sideplank.png",
-                  videos: [],
-                  procedure: ""
+                  videos: ["//www.youtube.com/embed/wqzrb67Dwf8", "//www.youtube.com/embed/_rdfjFSFKMY"],
+                  procedure: "Lie on your side, in a straight line from head to feet, resting on your forearm.\
+                              Your elbow should be directly under your shoulder.\
+                              With your abdominals gently contracted, lift your hips off the floor, maintaining the line.\
+                              Keep your hips square and your neck in line with your spine. Hold the position."
               }),
               duration: 30
           });
