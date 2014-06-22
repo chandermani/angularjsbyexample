851ae555ef8178c3acbd23e497476b4babe31853
chandermani <chandermani@outlook.com>
2014-06-22 09:58:52 +0530

    Added jumpingjack data

--- chapter2/app/js/7MinWorkout/workout.js
+++ chapter2/app/js/7MinWorkout/workout.js
@@ -90,10 +90,14 @@ angular.module('7minWorkout')
               details: new Exercise({
                   name: "jumpingJacks",
                   title: "Jumping Jacks",
-                  description: "Jumping Jacks.",
+                  description: "A jumping jack or star jump, also called side-straddle hop is a physical jumping exercise.",
                   image: "img/JumpingJacks.png",
-                  videos: [],
-                  procedure: ""
+                  videos: ["//www.youtube.com/embed/dmYwZH_BNd0", "//www.youtube.com/embed/BABOdJ-2Z6o", "//www.youtube.com/embed/c4DAnQ6DtF8"],
+                  procedure: "Assume an erect position, with feet together and arms at your side.\
+                            Slightly bend your knees, and propel yourself a few inches into the air.\
+                            While in air, bring your legs out to the side about shoulder width or slightly wider.\
+                            As you are moving your legs outward, you should raise your arms up over your head; arms should be slightly bent throughout the entire in-air movement.\
+                            Your feet should land shoulder width or wider as your hands meet above your head with arms slightly bent"
               }),
               duration: 30
           });
