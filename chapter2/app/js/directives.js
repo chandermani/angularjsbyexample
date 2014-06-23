2f164307d80a7ab21e061d011c2703981dcd587b
chandermani <chandermani@outlook.com>
2014-06-23 17:48:04 +0530

    Revert "Video Panel Height scroll fixed"

--- chapter2/app/js/directives.js
+++ chapter2/app/js/directives.js
@@ -8,19 +8,4 @@ angular.module('app.directives', []).
     return function(scope, elm, attrs) {
       elm.text(version);
     };
-  }])
-    .directive('appPanelHeight', function () {
-        return {
-            restrict: 'A',
-            link: function (scope, element, attrs) {
-                function setPanelHeight() {
-                    element.css("max-height", $(window).height() - (element.offset().top + 25));
-                }
-                setPanelHeight();
-                $(window).resize(function () {
-                    setPanelHeight();
-                });
-
-            }
-        };
-    });
+  }]);
