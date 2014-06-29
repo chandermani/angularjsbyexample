c6b0362e66b91ce8174297c839557b0af454a674
chandermani <chandermani@outlook.com>
2014-06-29 22:37:10 +0530

    Added initial audio support to app.

--- chapter2/app/js/7MinWorkout/workout.js
+++ chapter2/app/js/7MinWorkout/workout.js
@@ -3,7 +3,7 @@
 /* Controllers */
 
 angular.module('7minWorkout')
-  .controller('WorkoutController', ['$scope', '$interval', '$location', function ($scope, $interval, $location) {
+  .controller('WorkoutController', ['$scope', '$interval', '$location', '$timeout', function ($scope, $interval, $location, $timeout) {
       function WorkoutPlan(args) {
           this.exercises = [];
           this.name = args.name;
@@ -26,14 +26,19 @@ angular.module('7minWorkout')
           this.image = args.image;
           this.related = {};
           this.related.videos = args.videos;
-          this.nameSound = args.sound;
+          this.nameSound = args.nameSound;
           this.procedure = args.procedure;
       }
 
       var restExercise;
+      var currentExerciseIndex;
       var startWorkout = function () {
           $scope.workoutPlan = createWorkout();
           $scope.workoutTimeRemaining = $scope.workoutPlan.totalWorkoutDuration();
+          $scope.exercisesAudio = [];
+          angular.forEach($scope.workoutPlan.exercises, function (exercise) {
+              $scope.exercisesAudio.push({ src: exercise.details.nameSound, type: "audio/wav" });
+          });
           restExercise = {
               details: new Exercise({
                   name: "rest",
@@ -47,12 +52,24 @@ angular.module('7minWorkout')
               $scope.workoutTimeRemaining = $scope.workoutTimeRemaining - 1;
           }, 1000, $scope.workoutTimeRemaining);
 
+          currentExerciseIndex = -1;
           startExercise($scope.workoutPlan.exercises.shift());
       };
 
       var startExercise = function (exercisePlan) {
           $scope.currentExercise = exercisePlan;
           $scope.currentExerciseDuration = 0;
+
+          if (exercisePlan.details.name == 'rest') {
+              $scope.nextUpAudio.play();
+              $timeout(function () {
+                  $scope.nextUpExeciseAudio.play(currentExerciseIndex + 1, true);
+              }, 1000);
+          }
+          else {
+              currentExerciseIndex++;
+          }
+
           $interval(function () {
               ++$scope.currentExerciseDuration;
           }, 1000, $scope.currentExercise.duration)
@@ -103,6 +120,7 @@ angular.module('7minWorkout')
                   title: "Jumping Jacks",
                   description: "A jumping jack or star jump, also called side-straddle hop is a physical jumping exercise.",
                   image: "img/JumpingJacks.png",
+                  nameSound: "content/jumpingjacks.wav",
                   videos: ["//www.youtube.com/embed/dmYwZH_BNd0", "//www.youtube.com/embed/BABOdJ-2Z6o", "//www.youtube.com/embed/c4DAnQ6DtF8"],
                   procedure: "Assume an erect position, with feet together and arms at your side.\
                             <br/>Slightly bend your knees, and propel yourself a few inches into the air.\
@@ -118,6 +136,7 @@ angular.module('7minWorkout')
                   title: "Wall Sit",
                   description: "A wall sit, also known as a Roman Chair, is an exercise done to strengthen the quadriceps muscles.",
                   image: "img/wallsit.png",
+                  nameSound: "content/wallsit.wav",
                   videos: ["//www.youtube.com/embed/y-wV4Venusw", "//www.youtube.com/embed/MMV3v4ap4ro"],
                   procedure: "Place your back against a wall with your feet shoulder width apart and a little ways out from the wall.\
                               <br/>Then, keeping your back against the wall, lower your hips until your knees form right angles. "
@@ -130,6 +149,7 @@ angular.module('7minWorkout')
                   title: "Push Up",
                   description: "A push-up is a common exercise performed in a prone position by raising and lowering the body using the arms",
                   image: "img/pushup.png",
+                  nameSound: "content/pushups.wav",
                   videos: ["//www.youtube.com/embed/Eh00_rniF8E", "//www.youtube.com/embed/ZWdBqFLNljc", "//www.youtube.com/embed/UwRLWMcOdwI", "//www.youtube.com/embed/ynPwl6qyUNM", "//www.youtube.com/embed/OicNTT2xzMI"],
                   procedure: "Lie prone on the ground with hands placed as wide or slightly wider than shoulder width. \
                               Keeping the body straight, lower body to the ground by bending arms at the elbows. \
@@ -143,6 +163,7 @@ angular.module('7minWorkout')
                   title: "Abdominal Crunches",
                   description: "The basic crunch is a abdominal exercise in a strength-training program.",
                   image: "img/crunches.png",
+                  nameSound: "content/crunches.wav",
                   videos: ["//www.youtube.com/embed/Xyd_fa5zoEU", "//www.youtube.com/embed/MKmrqcoCZ-M"],
                   procedure: "Lie on your back with your knees bent and feet flat on the floor, hip-width apart.\
                               Place your hands behind your head so your thumbs are behind your ears.\
@@ -159,6 +180,7 @@ angular.module('7minWorkout')
                   title: "Step Up Onto Chair",
                   description: "Step exercises are ideal for building muscle in your lower body.",
                   image: "img/stepUpOntoChair.jpeg",
+                  nameSound: "content/stepup.wav",
                   videos: ["//www.youtube.com/embed/aajhW7DD1EA"],
                   procedure: "Position your chair in front of you.\
                               Stand with your feet about hip width apart, arms at your sides.\
@@ -173,6 +195,7 @@ angular.module('7minWorkout')
                   title: "Squat",
                   description: "The squat is a compound, full body exercise that trains primarily the muscles of the thighs, hips, buttocks and quads.",
                   image: "img/squat.png",
+                  nameSound: "content/squats.wav",
                   videos: ["//www.youtube.com/embed/QKKZ9AGYTi4", "//www.youtube.com/embed/UXJrBgI2RxA"],
                   procedure: "Stand with your head facing forward and your chest held up and out.\
                               Place your feet shoulder-width apart or little wider. Extend your hands straight out in front of you.\
@@ -188,6 +211,7 @@ angular.module('7minWorkout')
                   title: "Tricep Dips On Chair",
                   description: "A body weight exercise that targets triceps.",
                   image: "img/tricepdips.jpg",
+                  nameSound: "content/tricepdips.wav",
                   videos: ["//www.youtube.com/embed/tKjcgfu44sI", "//www.youtube.com/embed/jox1rb5krQI"],
                   procedure: "Sit up on a chair. Your legs should be slightly extended, with your feet flat on the floor.\
                               Place your hands edges of the chair. Your palms should be down, fingertips pointing towards the floor.\
@@ -202,6 +226,7 @@ angular.module('7minWorkout')
                   title: "Plank",
                   description: "The plank (also called a front hold, hover, or abdominal bridge) is an isometric core strength exercise that involves maintaining a difficult position for extended periods of time. ",
                   image: "img/plank.png",
+                  nameSound: "content/plank.wav",
                   videos: ["//www.youtube.com/embed/pSHjTRCQxIw", "//www.youtube.com/embed/TvxNkmjdhMM"],
                   procedure: "Get into pushup position on the floor.\
                               Bend your elbows 90 degrees and rest your weight on your forearms.\
@@ -216,6 +241,7 @@ angular.module('7minWorkout')
                   title: "High Knees",
                   description: "A form exercise that develops strength and endurance of the hip flexors and quads and stretches the hip extensors.",
                   image: "img/highknees.png",
+                  nameSound: "content/highknees.wav",
                   videos: ["//www.youtube.com/embed/OAJ_J3EZkdY", "//www.youtube.com/embed/8opcQdC-V-U"],
                   procedure: "Start standing with feet hip-width apart. \
                               Do inplace jog with your knees lifting as much as possible towards your chest."
@@ -228,6 +254,7 @@ angular.module('7minWorkout')
                   title: "Lunges",
                   description: "Lunges are a good exercise for strengthening, sculpting and building several muscles/muscle groups, including the quadriceps (or thighs), the gluteus maximus (or buttocks) as well as the hamstrings. ",
                   image: "img/lunges.png",
+                  nameSound: "content/lunge.wav",
                   videos: ["//www.youtube.com/embed/Z2n58m2i4jg"],
                   procedure: "Stand erect with your feet about one shoulder width apart.\
                               Put your hands on your hips, keep your back as straight as possible, relax your shoulders and keep your eyes facing directly ahead.\
@@ -244,6 +271,7 @@ angular.module('7minWorkout')
                   title: "Pushup And Rotate",
                   description: "A variation of pushup that requires you to rotate.",
                   image: "img/pushupNRotate.jpg",
+                  nameSound: "content/pushupandrotate.wav",
                   videos: ["//www.youtube.com/embed/qHQ_E-f5278"],
                   procedure: "Assume the classic pushup position, but as you come up, rotate your body so your right arm lifts up and extends overhead.\
                               Return to the starting position, lower yourself, then push up and rotate till your left hand points toward the ceiling."
@@ -256,6 +284,7 @@ angular.module('7minWorkout')
                   title: "Side Plank",
                   description: "A variation to Plank done using one hand only",
                   image: "img/sideplank.png",
+                  nameSound: "content/sideplank.wav",
                   videos: ["//www.youtube.com/embed/wqzrb67Dwf8", "//www.youtube.com/embed/_rdfjFSFKMY"],
                   procedure: "Lie on your side, in a straight line from head to feet, resting on your forearm.\
                               Your elbow should be directly under your shoulder.\
