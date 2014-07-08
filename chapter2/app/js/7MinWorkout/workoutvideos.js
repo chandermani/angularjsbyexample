e335e67fd4e78f63f5cee1afe76957c7823519c0
chandermani <chandermani@outlook.com>
2014-07-08 22:07:39 +0530

    Made video as images and open popup.

--- chapter2/app/js/7MinWorkout/workoutvideos.js
+++ chapter2/app/js/7MinWorkout/workoutvideos.js
@@ -3,20 +3,6 @@
 angular.module('7minWorkout')
   .controller('WorkoutVideosController', ['$scope', '$modal', function ($scope, $modal) {
       $scope.searchResults = [];
-      function YouTubeVideo(source) {
-          this.id = source.id.videoId;
-          this.thumbnail = source.thumbnails.high;
-          this.description = source.snippet.description;
-          this.publishedAt = source.snippet.publishedAt
-      }
-      $scope.findVideos = function () {
-          $http.get('https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=push+ups&key=AIzaSyCbX0oTRpY2BMLh1zPmfsyEAOQY31j2GPo').success(function (data) {
-              $scope.searchResults.length = 0;
-              angular.forEach(data.items, function (video) {
-                  $scope.searchResults.push(new YouTubeVideo(video));
-              });
-          });
-      };
       $scope.playVideo = function (videoId) {
           $scope.pauseWorkout();
           var dailog = $modal.open({
