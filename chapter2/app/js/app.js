c58b2b9f3adf91f7e358a2c3cbf87f6c721baf5f
chandermani <chandermani@outlook.com>
2014-07-20 18:31:11 +0530

    Added animation

--- chapter2/app/js/app.js
+++ chapter2/app/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('app', ['ngRoute', 'ngSanitize', '7minWorkout', 'mediaPlayer', 'ui.bootstrap']).
+angular.module('app', ['ngRoute', 'ngSanitize', '7minWorkout', 'mediaPlayer', 'ui.bootstrap', 'ngAnimate']).
 config(function ($routeProvider, $sceDelegateProvider) {
     $routeProvider.when('/start', { templateUrl: 'partials/start.html' });
     $routeProvider.when('/workout', { templateUrl: 'partials/workout.html', controller: 'WorkoutController' });
