f6db4a93b14b4b716c691bf829c1aff30d46b84b
chandermani <chandermani@outlook.com>
2014-08-15 17:31:42 +0530

    Added formatter and parser for number to integer parsing.

--- chapter2/app/js/WorkoutBuilder/workout.js
+++ chapter2/app/js/WorkoutBuilder/workout.js
@@ -38,6 +38,19 @@ angular.module('WorkoutBuilder')
           }
       });
 
+      //var restWatch = $scope.$watch('formWorkout.restBetweenExercise', function (newValue) {
+      //    // Conversion logic courtesy http://stackoverflow.com/questions/596467/how-do-i-convert-a-number-to-an-integer-in-javascript
+      //    if (newValue) {
+      //        newValue.$parsers.unshift(function (value) {
+      //            return isNaN(parseInt(value)) ? value : parseInt(value);
+      //        });
+      //        newValue.$formatters.push(function (value) {
+      //            return isNaN(parseInt(value)) ? value : parseInt(value);
+      //        });
+      //        restWatch(); //De-register the watch.
+      //    }
+      //});
+
       $scope.moveExerciseTo = function (exercise, location) {
           WorkoutBuilderService.moveExerciseTo(exercise, location);
       };
