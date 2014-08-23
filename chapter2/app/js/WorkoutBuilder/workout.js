c613fd0a722a60a775ecae1625444ab9eb7215a6
chandermani <chandermani@outlook.com>
2014-08-23 17:31:00 +0530

    Added the formatter and parser example

--- chapter2/app/js/WorkoutBuilder/workout.js
+++ chapter2/app/js/WorkoutBuilder/workout.js
@@ -23,6 +23,19 @@ angular.module('WorkoutBuilder')
           $scope.formWorkout.$setPristine();
       }
 
+      var restWatch = $scope.$watch('formWorkout.restBetweenExercise', function (newValue) {
+          // Conversion logic courtesy http://stackoverflow.com/questions/596467/how-do-i-convert-a-number-to-an-integer-in-javascript
+          if (newValue) {
+              newValue.$parsers.unshift(function (value) {
+                  return isNaN(parseInt(value)) ? value : parseInt(value);
+              });
+              newValue.$formatters.push(function (value) {
+                  return isNaN(parseInt(value)) ? value : parseInt(value);
+              });
+              restWatch(); //De-register the watch.
+          }
+      });
+
       $scope.moveExerciseTo = function (exercise, location) {
           WorkoutBuilderService.moveExerciseTo(exercise, location);
       };
@@ -37,6 +50,7 @@ angular.module('WorkoutBuilder')
       }
       var init = function () {
           $scope.workout = selectedWorkout;
+          $scope.workout.restBetweenExercise = 25.53;
       };
       init();
   }]);
