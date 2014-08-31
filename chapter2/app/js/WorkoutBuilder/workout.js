11b6e68918b6daa27773d15bfde3b06500742c0e
chandermani <chandermani@outlook.com>
2014-08-31 21:59:29 +0530

    Fixed getworkouts, using then

--- chapter2/app/js/WorkoutBuilder/workout.js
+++ chapter2/app/js/WorkoutBuilder/workout.js
@@ -6,8 +6,8 @@ angular.module('WorkoutBuilder')
           $location.path('/builder/workouts/' + workout.name);
       }
       var init = function () {
-          WorkoutService.getWorkouts().success(function (workouts) {
-              $scope.workouts = workouts;
+          WorkoutService.getWorkouts().then(function (data) {
+              $scope.workouts = data;
           });
       };
       init();
