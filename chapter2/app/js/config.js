cc1db6c0bd4bc050e36892033dc0050b74e11f57
chandermani <chandermani@outlook.com>
2014-09-09 22:07:28 +0530

    Fixed the resolve in workout edit route

--- chapter2/app/js/config.js
+++ chapter2/app/js/config.js
@@ -41,7 +41,11 @@ config(function ($routeProvider, $sceDelegateProvider, WorkoutServiceProvider) {
         topNav: 'partials/workoutbuilder/top-nav.html',
         resolve: {
             selectedWorkout: ['WorkoutBuilderService', '$route', '$location', function (WorkoutBuilderService, $route, $location) {
-                return WorkoutBuilderService.startBuilding($route.current.params.id);
+                var workout = WorkoutBuilderService.startBuilding($route.current.params.id);
+                if (!workout) {
+                    $location.path('/builder/workouts');    //If the workout not found redirect to workout list
+                }
+                return workout;
             }],
         }
     });
