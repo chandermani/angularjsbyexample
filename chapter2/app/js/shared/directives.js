9854d6db42ae879540d3c9b922f092f0385d5730
chandermani <chandermani@outlook.com>
2014-09-22 07:23:41 +0530

    Added remove validation and clue.

--- chapter2/app/js/shared/directives.js
+++ chapter2/app/js/shared/directives.js
@@ -18,17 +18,24 @@ angular.module('app').directive('ngConfirm', [function () {
 angular.module('app').directive('remoteValidator', ['$parse', function ($parse) {
     return {
         restrict: 'A',
-        require: 'ngModel',
-        link: function (scope, elm, attr, ngModelCtrl) {
+        require: ['ngModel', '?^remoteValidatorClues'],
+        link: function (scope, elm, attr, ctrls) {
             var expfn = $parse(attr["remoteValidatorFunction"]);
             var validatorName = attr["remoteValidator"];
-            ngModelCtrl.$parsers.push(function (value) {
+            var modelCtrl = ctrls[0];
+            var clueCtrl = ctrls[1];
+            modelCtrl.$parsers.push(function (value) {
                 var result = expfn(scope, { 'value': value });
                 if (result.then) {
+                    if (clueCtrl) clueCtrl.showClue();
                     result.then(function (data) { //For promise type result object
-                        ngModelCtrl.$setValidity(validatorName, data);
+                        console.log('hiding spinner');
+                        if (clueCtrl) clueCtrl.hideClue();
+                        modelCtrl.$setValidity(validatorName, data);
                     }, function (error) {
-                        ngModelCtrl.$setValidity(validatorName, false);
+                        console.log('hiding spinner');
+                        if (clueCtrl) clueCtrl.hideClue();
+                        modelCtrl.$setValidity(validatorName, false);
                     });
                 }
                 return value;
@@ -44,7 +51,6 @@ angular.module('app').directive('updateOnBlur', function () {
         priority: '100',
         link: function (scope, elm, attr, ngModelCtrl) {
             if (attr.type === 'radio' || attr.type === 'checkbox') return;
-
             elm.unbind('input').unbind('keydown').unbind('change');
             elm.bind('blur', function () {
                 scope.$apply(function () {
@@ -55,3 +61,17 @@ angular.module('app').directive('updateOnBlur', function () {
     };
 });
 
+angular.module('app').directive('remoteValidatorClues', ['$compile', function ($compile) {
+    return {
+        scope: true,
+        link: function (scope, element, attr) {
+            var e = $compile('<div><label ng-if="busy" class="text-info">checking...</label></div>')(scope);
+            element.append(e);
+        },
+        controller: ['$scope', function ($scope) {
+            this.showClue = function () { $scope.busy = true; }
+            this.hideClue = function () { $scope.busy = false; }
+        }]
+
+    }
+}]);
