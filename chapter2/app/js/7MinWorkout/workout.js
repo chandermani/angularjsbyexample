1eb10d8400d0476cddb2f6ad6f5dd08a60f5a656
chandermani <chandermani@outlook.com>
2014-10-03 20:04:57 +0530

    Fixed next exercise calculation

--- chapter2/app/js/7MinWorkout/workout.js
+++ chapter2/app/js/7MinWorkout/workout.js
@@ -78,7 +78,7 @@ angular.module('7minWorkout')
               nextExercise = $scope.workoutPlan.exercises[$scope.currentExerciseIndex + 1];
           }
           else {
-              if ($scope.currentExerciseIndex == $scope.workoutPlan.exercises - 1) {
+              if ($scope.currentExerciseIndex < $scope.workoutPlan.exercises.length - 1) {
                   nextExercise = restExercise;
               }
           }
