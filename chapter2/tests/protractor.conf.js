60f406ea7e87b79657c0528297c118e18cdfa3b5
chandermani <chandermani@outlook.com>
2014-10-14 09:46:31 +0530

    Fixed test folder name and added protractor config.

---
+++ chapter2/tests/protractor.conf.js
@@ -0,0 +1,89 @@
+exports.config = {
+  
+
+  // ---------------------------------------------------------------------------
+  // ----- What tests to run ---------------------------------------------------
+  // ---------------------------------------------------------------------------
+
+  // Spec patterns are relative to the location of this config.
+  specs: [
+    'spec/*_spec.js',
+  ],
+
+  // Patterns to exclude.
+  exclude: [],
+
+  // ---------------------------------------------------------------------------
+  // ----- How to set up browsers ----------------------------------------------
+  // ---------------------------------------------------------------------------
+  //
+  // Protractor can launch your tests on one or more browsers. If you are
+  // testing on a single browser, use the capabilities option. If you are
+  // testing on multiple browsers, use the multiCapabilities array.
+
+  // For a list of available capabilities, see
+  // https://code.google.com/p/selenium/wiki/DesiredCapabilities
+  //
+  // In addition, you may specify count, shardTestFiles, and maxInstances.
+  capabilities: {
+    browserName: 'chrome',
+
+    // Number of times to run this set of capabilities (in parallel, unless
+    // limited by maxSessions). Default is 1.
+    count: 1,
+
+    // If this is set to be true, specs will be sharded by file (i.e. all
+    // files to be run by this set of capabilities will run in parallel).
+    // Default is false.
+    shardTestFiles: false,
+
+    // Maximum number of browser instances that can run in parallel for this
+    // set of capabilities. This is only needed if shardTestFiles is true.
+    // Default is 1.
+    maxInstances: 1,
+
+    // Additional spec files to be run on this capability only.
+    specs: ['spec/chromeOnlySpec.js'],
+
+    // Spec files to be excluded on this capability only.
+    exclude: ['spec/doNotRunInChromeSpec.js']
+
+  },
+
+  // If you would like to run more than one instance of WebDriver on the same
+  // tests, use multiCapabilities, which takes an array of capabilities.
+  // If this is specified, capabilities will be ignored.
+  multiCapabilities: [],
+
+  // ---------------------------------------------------------------------------
+  // ----- Global test information ---------------------------------------------
+  // ---------------------------------------------------------------------------
+  //
+  // A base URL for your application under test. Calls to protractor.get()
+  // with relative paths will be prepended with this.
+  baseUrl: 'http://localhost:54963/index.html',
+
+  // CSS Selector for the element housing the angular app - this defaults to
+  // body, but is necessary if ng-app is on a descendant of <body>.
+  rootElement: 'body',
+
+  // The params object will be passed directly to the Protractor instance,
+  // and can be accessed from your test as browser.params. It is an arbitrary
+  // object and can contain anything you may need in your test.
+  // This can be changed via the command line as:
+  //   --params.login.user 'Joe'
+  params: {
+    login: {
+      user: 'Jane',
+      password: '1234'
+    }
+  },
+    // Require files before executing the features.
+    require: 'cucumber/stepDefinitions.js',
+    // Only execute the features or scenarios with tags matching @dev.
+    // This may be an array of strings to specify multiple tags to include.
+    tags: '@dev',
+    // How to format features (default: progress)
+    format: 'summary'
+  }
+};
