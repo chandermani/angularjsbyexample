b0749def8b1dbc9fd334a1179ceac53ffb2420b9
chandermani <chandermani@outlook.com>
2014-10-24 11:26:24 +0530

    Added initial protractor tests.

--- trainer/tests/e2e/workout-runner.js
+++ trainer/tests/e2e/workout-runner.js
@@ -1,12 +1,23 @@
-describe("Workout Runner",function() {
-	describe("Start Page",function() {
-		beforeEach(function() {
-			browser.get("");
-		});
-		it("should load the start page.",function(){
-			expect(browser.getTitle()).toBe("Personal Trainer");
-			expect(element(by.id("start")).getText()).toBe("Select Workout");
-		});
-		
-	});
+describe("Workout Runner", function () {
+    describe("Start Page", function () {
+        beforeEach(function () {
+            browser.get("");
+        });
+        it("should load the start page.", function () {
+            expect(browser.getTitle()).toBe("Personal Trainer");
+            expect(element(by.id("start")).getText()).toBe("Select Workout");
+        });
+
+        it("should have a search workout.", function () {
+            var filteredWorkouts = element.all(by.repeater("workout in workouts"));
+            expect(filteredWorkouts.count()).toEqual(2);
+
+            var searchInput = element(by.model("workoutSearch"));
+            searchInput.sendKeys("test");
+
+            expect(filteredWorkouts.count()).toEqual(1);
+            expect(filteredWorkouts.first().element(by.css(".title")).getText()).toBe("A test Workout");
+        });
+
+    });
 });
