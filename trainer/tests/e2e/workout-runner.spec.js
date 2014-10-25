3c5083c9e9598ff604a2e2ef2e1bbfaaee90af1c
chandermani <chandermani@outlook.com>
2014-10-25 16:11:47 +0530

    Added unit test for workout-runner

--- trainer/tests/e2e/workout-runner.spec.js
+++ trainer/tests/e2e/workout-runner.spec.js
@@ -8,7 +8,7 @@ describe("Workout Runner", function () {
             expect(element(by.id("start")).getText()).toBe("Select Workout");
         });
 
-        it("should have a search workout.", function () {
+        it("should search workout with specific name.", function () {
             var filteredWorkouts = element.all(by.repeater("workout in workouts"));
             expect(filteredWorkouts.count()).toEqual(2);
 
@@ -19,5 +19,21 @@ describe("Workout Runner", function () {
             expect(filteredWorkouts.first().element(by.css(".title")).getText()).toBe("A test Workout");
         });
 
+        it("should navigate to workout runner.", function () {
+            var filteredWorkouts = element.all(by.repeater("workout in workouts"));
+            filteredWorkouts.first().click();
+            expect(browser.getLocationAbsUrl()).toContain("/workout/7minworkout");
+        });
+
+    });
+
+    describe("Workout Runner page", function () {
+        beforeEach(function () {
+            browser.get("#/workout/7minworkout");
+        });
+
+        iit("should load workout data", function () {
+            expect(element(by.id("description-panel")).element(by.css(".panel-title")).getText()).toBe("Description");
+        });
     });
 });
