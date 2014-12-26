97d8a876f0586ee917dfe6f6ad641ebebb1c49ee
chandermani <chandermani@outlook.com>
2014-12-26 19:09:23 +0530

    Added ng-messages based validation message, to showcase capabilities.

--- trainer/app/js/WorkoutBuilder/workout.js
+++ trainer/app/js/WorkoutBuilder/workout.js
@@ -17,18 +17,18 @@ angular.module('WorkoutBuilder')
           WorkoutBuilderService.removeExercise(exercise);
       };
 
-      var restWatch = $scope.$watch('formWorkout.restBetweenExercise', function (newValue) {
-          // Conversion logic courtesy http://stackoverflow.com/questions/596467/how-do-i-convert-a-number-to-an-integer-in-javascript
-          if (newValue) {
-              newValue.$parsers.unshift(function (value) {
-                  return isNaN(parseInt(value)) ? value : parseInt(value);
-              });
-              newValue.$formatters.push(function (value) {
-                  return isNaN(parseInt(value)) ? value : parseInt(value);
-              });
-              restWatch(); //De-register the watch.
-          }
-      });
+      //var restWatch = $scope.$watch('formWorkout.restBetweenExercise', function (newValue) {
+      //    // Conversion logic courtesy http://stackoverflow.com/questions/596467/how-do-i-convert-a-number-to-an-integer-in-javascript
+      //    if (newValue) {
+      //        newValue.$parsers.unshift(function (value) {
+      //            return isNaN(parseInt(value)) ? value : parseInt(value);
+      //        });
+      //        newValue.$formatters.push(function (value) {
+      //            return isNaN(parseInt(value)) ? value : parseInt(value);
+      //        });
+      //        restWatch(); //De-register the watch.
+      //    }
+      //});
 
       $scope.moveExerciseTo = function (exercise, location) {
           WorkoutBuilderService.moveExerciseTo(exercise, location);
--- trainer/app/js/app.js
+++ trainer/app/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('app', ['ngRoute', 'ngSanitize', '7minWorkout', 'WorkoutBuilder', 'mediaPlayer', 'ui.bootstrap', 'LocalStorageModule', 'ngAnimate']).
+angular.module('app', ['ngRoute', 'ngSanitize', '7minWorkout', 'WorkoutBuilder', 'mediaPlayer', 'ui.bootstrap', 'LocalStorageModule', 'ngAnimate', 'ngMessages']).
 config(function ($routeProvider, $sceDelegateProvider) {
     $routeProvider.when('/start', { templateUrl: 'partials/start.html' });
     $routeProvider.when('/workout', { templateUrl: 'partials/workout.html', controller: 'WorkoutController' });
