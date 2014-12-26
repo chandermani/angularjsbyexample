6f1d4cd31c28a977996f2334a81b24bb44f351da
chandermani <chandermani@outlook.com>
2014-12-26 19:09:23 +0530

    Added ng-messages based validation message, to showcase capabilities.

--- trainer/app/js/app.js
+++ trainer/app/js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('app', ['ngRoute', 'ngSanitize', '7minWorkout', 'WorkoutBuilder', 'ui.bootstrap', 'LocalStorageModule', 'ngAnimate', 'ngResource', 'oc.lazyLoad', 'pascalprecht.translate']);
+angular.module('app', ['ngRoute', 'ngSanitize', '7minWorkout', 'WorkoutBuilder', 'ui.bootstrap', 'LocalStorageModule', 'ngAnimate','ngMessages', 'ngResource', 'oc.lazyLoad', 'pascalprecht.translate']);
 
 angular.module('7minWorkout', []);
 angular.module('WorkoutBuilder', []);
