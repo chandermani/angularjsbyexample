c0e0097690926aa91f8fb60cac1f8956f60fc134
chandermani <chandermani@outlook.com>
2014-12-26 11:28:35 +0530

    Changed exercise duration to dropdown.

--- trainer/app/js/WorkoutBuilder/workout.js
+++ trainer/app/js/WorkoutBuilder/workout.js
@@ -34,6 +34,27 @@ angular.module('WorkoutBuilder')
           WorkoutBuilderService.moveExerciseTo(exercise, location);
       };
 
+      $scope.durations = [{ title: "15 seconds", value: 15 },
+                         { title: "30 seconds", value: 30 },
+                         { title: "45 seconds", value: 45 },
+                         { title: "1 minute", value: 60 },
+                         { title: "1 minute 15 seconds", value: 75 },
+                         { title: "1 minute 30 seconds", value: 90 },
+                         { title: "1 minute 45 seconds", value: 105 },
+                         { title: "2 minutes", value: 120 },
+                         { title: "2 minutes 15 seconds", value: 135 },
+                         { title: "2 minutes 30 seconds", value: 150 },
+                         { title: "2 minutes 45 seconds", value: 165 },
+                         { title: "3 minutes", value: 180 },
+                         { title: "3 minutes 15 seconds", value: 195 },
+                         { title: "3 minutes 30 seconds", value: 210 },
+                         { title: "3 minutes 45 seconds", value: 225 },
+                         { title: "4 minutes", value: 240 },
+                         { title: "4 minutes 15 seconds", value: 255 },
+                         { title: "4 minutes 30 seconds", value: 270 },
+                         { title: "4 minutes 45 seconds", value: 285 },
+                         { title: "5 minutes", value: 300 }];
+
       var init = function () {
           $scope.workout = selectedWorkout;
       };
