9ca1b3db140f223bfbedc7102ea37c90033fa4b6
chandermani <chandermani@outlook.com>
2015-01-04 10:56:36 +0530

    Fixed error message showing logic.

--- trainer/app/js/root.js
+++ trainer/app/js/root.js
@@ -21,16 +21,18 @@ angular.module('app')
       };
       WorkoutHistoryController['$inject'] = ['$scope', '$modalInstance', 'workoutHistoryTracker'];
 
-      $scope.$on('$routeChangeSuccess', function (e, current,previous) {
+      $scope.$on('$routeChangeSuccess', function (event, current, previous) {
           $scope.currentRoute = current;
           $scope.routeHasError = false;
       });
 
-      $scope.$on('$routeChangeError', function (e, current, previous) {
-          $scope.routeHasError = true;
-          $scope.routeError = current.routeErrorMessage;
+      $scope.$on('$routeChangeError', function (event, current, previous, error) {
+          if (error.status === 404 && current.originalPath === "/builder/workouts/:id") {
+              $scope.routeHasError = true;
+              $scope.routeError = current.routeErrorMessage;
+          }
 
       });
-      
+
 
   }]);
