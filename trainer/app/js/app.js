952b4fcf0f07322b44ab7b1807693a8650f4e486
chandermani <chandermani@outlook.com>
2018-02-25 12:35:11 +0000

    Fixed package and gitignore

--- trainer/app/js/app.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-angular.module('app', ['ngRoute', 'ngSanitize', '7minWorkout', 'WorkoutBuilder', 'mediaPlayer', 'ui.bootstrap', 'LocalStorageModule', 'ngAnimate','ngResource','ngMessages','pascalprecht.translate']);
-
-angular.module('7minWorkout', []);
-angular.module('WorkoutBuilder', []);
