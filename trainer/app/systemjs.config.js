bd095dccc873ad73cbeb434991ce6a38a69a93ec
chandermani <chandermani@outlook.com>
2018-02-25 12:24:02 +0000

    Fixed systemjs
    (cherry picked from commit 6bd89dec3c1546cd85a5111eb35f5cbd2d5a7a0f)

--- trainer/app/systemjs.config.js
+++ trainer/app/systemjs.config.js
@@ -1,34 +1,42 @@
+/**
+ * System configuration for Angular samples
+ * Adjust as necessary for your application needs.
+ */
 (function (global) {
-    var map = {
+    System.config({
+      paths: {
+        // paths serve as alias
+        'npm:': 'node_modules/'
+      },
+      // map tells the System loader where to look for things
+      map: {
+        // our app is within the app folder
         'app': 'js',
-        '@angular': '/node_modules/@angular',
-        '@angular/upgrade/static': '/node_modules/@angular/upgrade',
-        'rxjs': '/node_modules/rxjs'
-    };
-    var packages = {
-        'app': {
-            main: 'main.js',
-            defaultExtension: 'js'
+  
+        // angular bundles
+        '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+        '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+        '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+        '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+        '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+        '@angular/common/http': 'npm:@angular/common/bundles/common-http.umd.js',
+        '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+        '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+        '@angular/upgrade/static': 'npm:@angular/upgrade/bundles/upgrade-static.umd.js',
+        'tslib':'npm:tslib/tslib.js',
+  
+        // other libraries
+        'rxjs':                      'npm:rxjs',
+      },
+      // packages tells the System loader how to load when no filename and/or no extension
+      packages: {
+        app: { 
+            main:'main.js', defaultExtension: 'js',
         },
-        'rxjs': {
-            defaultExtension: 'js'
-        },
-        
-        '@angular/core': {main: '/bundles/core.umd.js', defaultExtension:'js'},
-        '@angular/common': {main: '/bundles/common.umd.js', defaultExtension:'js'},
-        '@angular/compiler': {main: '/bundles/compiler.umd.js', defaultExtension:'js'},
-        '@angular/platform-browser': {main: '/bundles/platform-browser.umd.js', defaultExtension:'js'},
-        '@angular/platform-browser-dynamic': {main: '/bundles/platform-browser-dynamic.umd.js', defaultExtension:'js'},
-        '@angular/http': {main: '/bundles/http.umd.js', defaultExtension:'js'},
-        '@angular/router': {main: '/bundles/router.umd.js', defaultExtension:'js'},
-        '@angular/forms': {main: '/bundles/forms.umd.js', defaultExtension:'js'},
-        '@angular/upgrade/static': {main: '/bundles/upgrade-static.umd.js', defaultExtension:'js'}
-    };
-    
-    var config = {
-        map: map,
-        packages: packages
-    };
-
-    System.config(config);
-})(this);
+        rxjs: {
+          defaultExtension: 'js'
+        }
+      }
+    });
+  })(this);
+  
