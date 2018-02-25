5e93d02a7bd42b7e7b04e856b57a17bb1593b55c
chandermani <chandermani@outlook.com>
2018-02-25 12:35:11 +0000

    Fixed package and gitignore
    (cherry picked from commit 952b4fcf0f07322b44ab7b1807693a8650f4e486)
    
    # Conflicts:
    #	trainer/app/js/app-ng1.module.js
    #	trainer/app/package.json

--- trainer/app/js/app-ng1.module.js
+++
@@ -1,4 +0,0 @@
-angular.module('app', ['ngRoute', 'ngSanitize', '7minWorkout', 'WorkoutBuilder', 'mediaPlayer', 'ui.bootstrap', 'LocalStorageModule', 'ngAnimate','ngResource','ngMessages','pascalprecht.translate']);
-
-angular.module('7minWorkout', []);
-angular.module('WorkoutBuilder', []);
