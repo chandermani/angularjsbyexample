cb0b660ba40566f1fbd2b1b70a835a15e2ddcab4
chandermani <chandermani@outlook.com>
2018-05-17 06:35:22 +0100

    Upgraded to angular 6!
    (cherry picked from commit dbe36c6b86ae0e4d4bfe6e0110e68ec3fdc16d38)

--- trainer/app/systemjs.config.js
+++ trainer/app/systemjs.config.js
@@ -3,40 +3,46 @@
  * Adjust as necessary for your application needs.
  */
 (function (global) {
-    System.config({
-      paths: {
-        // paths serve as alias
-        'npm:': 'node_modules/'
+  System.config({
+    paths: {
+      // paths serve as alias
+      'npm:': 'node_modules/'
+    },
+    // map tells the System loader where to look for things
+    map: {
+      // our app is within the app folder
+      'app': 'js',
+
+      // angular bundles
+      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/common/http': 'npm:@angular/common/bundles/common-http.umd.js',
+      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      '@angular/upgrade/static': 'npm:@angular/upgrade/bundles/upgrade-static.umd.js',
+      'tslib': 'npm:tslib/tslib.js',
+
+      // other libraries
+      'rxjs': 'npm:rxjs',
+      'rxjs/operators': 'npm:rxjs/operators',
+    },
+    // packages tells the System loader how to load when no filename and/or no extension
+    packages: {
+      app: {
+        main: 'main.js',
+        defaultExtension: 'js',
       },
-      // map tells the System loader where to look for things
-      map: {
-        // our app is within the app folder
-        'app': 'js',
-  
-        // angular bundles
-        '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-        '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-        '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-        '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-        '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-        '@angular/common/http': 'npm:@angular/common/bundles/common-http.umd.js',
-        '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-        '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-        '@angular/upgrade/static': 'npm:@angular/upgrade/bundles/upgrade-static.umd.js',
-        'tslib':'npm:tslib/tslib.js',
-  
-        // other libraries
-        'rxjs':                      'npm:rxjs',
+      rxjs: {
+        main: "index.js",
+        defaultExtension: 'js'
       },
-      // packages tells the System loader how to load when no filename and/or no extension
-      packages: {
-        app: { 
-            main:'main.js', defaultExtension: 'js',
-        },
-        rxjs: {
-          defaultExtension: 'js'
-        }
+      "rxjs/operators": {
+        main: "index.js",
+        defaultExtension: "js"
       }
-    });
-  })(this);
-  
+    }
+  });
+})(this);
